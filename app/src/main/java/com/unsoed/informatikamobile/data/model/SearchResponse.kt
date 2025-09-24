5d0c08fafc4e6663899e684d88136107b7652223
IrfanRomadhonWidodo <irfan.widodo@mhs.unsoed.ac.id>
2025-09-24 15:36:44 +0700

    Tugas 3 Pertemuan 4

--- app/src/main/java/com/unsoed/informatikamobile/data/model/SearchResponse.kt
+++ app/src/main/java/com/unsoed/informatikamobile/data/model/SearchResponse.kt
@@ -1,3 +1,20 @@
 package com.unsoed.informatikamobile.data.model
 
-data class SearchResponse()
+import com.google.gson.annotations.SerializedName
+
+data class SearchResponse(
+    @SerializedName("docs")
+    val docs: List<BookDoc>
+)
+
+data class BookDoc(
+    @SerializedName("title")
+    val title: String?,
+
+    @SerializedName("author_name")
+    val authorName: List<String>?,
+
+    @SerializedName("first_publish_year")
+    val firstPublishYear: Int?
+)
+
--- app/src/main/java/com/unsoed/informatikamobile/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/unsoed/informatikamobile/viewmodel/MainViewModel.kt
@@ -1,4 +1,35 @@
 package com.unsoed.informatikamobile.viewmodel
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import kotlinx.coroutines.launch
+import android.util.Log
+import retrofit2.Response
+import com.unsoed.informatikamobile.data.network.RetrofitInstance
+import com.unsoed.informatikamobile.data.model.BookDoc
+
+
+class MainViewModel : ViewModel() {
+
+    private val _books = MutableLiveData<List<BookDoc>>()
+    val books: LiveData<List<BookDoc>> = _books
+
+    fun fetchBooks(query: String) {
+        viewModelScope.launch {
+            try {
+                val response = RetrofitInstance.api.searchBooks(query, limit = 10)
+                if (response.isSuccessful) {
+                    val result = response.body()?.docs ?: emptyList()
+                    _books.value = result
+                    Log.d("SUCCESS_GET_DATA", "$result")
+                } else {
+                    Log.e("API_ERROR", "${response.code()} ${response.message()}")
+                }
+            } catch (e: Exception) {
+                Log.e("API_EXCEPTION", e.localizedMessage ?: "Unknown error")
+            }
+        }
+    }
 }
